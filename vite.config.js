111d8082a80884859b46a0c7db96f2b7cf6c13f9
uksir <yeah126139163@163.com>
2025-02-28 07:47:43 +0800

    添加自动部署

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/-Face-Fortune-/' : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
